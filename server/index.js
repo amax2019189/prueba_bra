e154f6faf6476d97a2491441bafa9d5f0478d25a
Braulio Echeverria <braulioecheverria@kinal.org.gt>
2026-09-18 23:20:35 -0600

    finish him

--- server/index.js
+++ server/index.js
@@ -4,6 +4,4 @@ import { initServer } from './configs/app.js';
 config();
 initServer();
 
-// probando será que ahora si 
-// haciendo una prueba ya con todo el flujo de trabajo, a ver si ahora si funciona todo correctamente
-// holisssssssssssssssssss
+// probando será que ahora si
